ffa9f4ae8cb92668f0863978eba393dd5760a191
Michel <michelv1994@caiway.net>
2015-12-01 15:16:40 +0100

    Oke

--- EntryPoint/Program.cs
+++ EntryPoint/Program.cs
@@ -41,7 +41,7 @@ namespace EntryPoint
     {
 
             IEnumerable<Vector2> yolofucker;
-            Console.WriteLine("Heyy");
+            Console.WriteLine("Heyyy");
             
             //Vector3.
             return specialBuildings.OrderBy(v => Vector2.Distance(v, house));
